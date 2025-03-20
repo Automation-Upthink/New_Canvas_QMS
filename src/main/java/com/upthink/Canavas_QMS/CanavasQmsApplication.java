c2736483a92c4497c8eab914b042a55f75aa83d5
Sreenjay Sen <SreenjaySen@Admins-MacBook-Pro.local>
2025-03-20 10:27:34 +0530

    First Commit

---
+++ src/main/java/com/upthink/Canavas_QMS/CanavasQmsApplication.java
@@ -0,0 +1,13 @@
+package com.upthink.Canavas_QMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanavasQmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanavasQmsApplication.class, args);
+	}
+
+}
